2c6bd3003350cf65400cfabff73eb0d4d384ad92
Kaustubh Singh <96336281+kaustubhsingh47@users.noreply.github.com>
2023-05-31 01:06:45 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "rg_name" {
+  type        = string
+  description = "The type of resource to create in Azure (e.g. 'aks/vms')."
+}
+
+variable "location" {
+  type        = string
+  description = "The type of resource to create in Azure (e.g. 'aks/vms')."
+}
+
+variable "lock_name" {
+  type        = string
+  description = "The type of resource to create in Azure (e.g. 'aks/vms')."
+}
+
+
+variable "notes" {
+  type        = string
+  description = "The type of resource to create in Azure (e.g. 'aks/vms')."
+}
+
