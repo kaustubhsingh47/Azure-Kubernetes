c51ab0e197c1e4943bc4a1ffb610047dc32e87c7
Kaustubh Singh <96336281+kaustubhsingh47@users.noreply.github.com>
2023-07-26 01:36:02 +0530

    Delete modules directory

--- modules/rg/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "az_rg" {
-  name                      = "${var.resource_group_name}-${var.client_name}-${var.environment}"
-  location                  = var.resource_group_location
-  tags                      = {
-      environment           = var.environment
-      client                = var.client_name
-  }
-}
